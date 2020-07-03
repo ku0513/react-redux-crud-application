754275f627a2ca8f51f88aa821f8653d5062cd2f
ku0513 <kenichi.umeda@gmail.com>
2020-07-04 04:51:25 +0900

    implement event new component

--- src/components/events_index.js
+++ src/components/events_index.js
@@ -1,44 +1,54 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import _ from 'lodash';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import _ from "lodash";
+import { Link } from "react-router-dom";
 
-import { readEvents } from '../actions';
+import { readEvents } from "../actions";
 
 class EventsIndex extends Component {
-
   componentDidMount() {
     this.props.readEvents();
-  };
+  }
 
   renderEvents() {
-    return _.map(this.props.events, event => (
+    return _.map(this.props.events, event =>
       <tr key={event.id}>
-        <td>{event.id}</td>
-        <td>{event.title}</td>
-        <td>{event.body}</td>
+        <td>
+          {event.id}
+        </td>
+        <td>
+          {event.title}
+        </td>
+        <td>
+          {event.body}
+        </td>
       </tr>
-    ))
-  };
+    );
+  }
 
   render() {
     return (
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Title</th>
-            <th>Body</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.renderEvents()}
-        </tbody>
-      </table>
+      <React.Fragment>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Title</th>
+              <th>Body</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.renderEvents()}
+          </tbody>
+        </table>
+
+        <Link to="/events/new">New Event</Link>
+      </React.Fragment>
     );
-  };
-};
+  }
+}
 
-const mapStateToProps = state => ({ events: state.events});
-const mapDispacthToProps = ({ readEvents });
+const mapStateToProps = state => ({ events: state.events });
+const mapDispacthToProps = { readEvents };
 
-export default connect(mapStateToProps, mapDispacthToProps)(EventsIndex)
+export default connect(mapStateToProps, mapDispacthToProps)(EventsIndex);
